276bafcb41039bad84d4ebd8c37f710e44f5146f
Aaron <atsand23@gmail.com>
2018-02-09 13:34:58 -0500

    added product class

---
+++ Midterm/Product.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Midterm
+{
+    class Product
+    {
+        private string Name { get; set; }
+        private string Category { get; set; }
+        private string Description { get; set; }
+        private string Price { get; set; }
+
+        public Product(string name, string category, string description, string price)
+        {
+            Name = name;
+            Category = category;
+            Description = description;
+            Price = price;
+        }
+    }
+}
