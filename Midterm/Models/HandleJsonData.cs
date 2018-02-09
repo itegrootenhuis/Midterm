aba8673094e7305ec828177f591e04baaaac700b
itegrootenhuis <itegrootenhuis@bizstream.com>
2018-02-09 18:38:46 -0500

    Added Newtonsoft & working on getting json data.

---
+++ Midterm/Models/HandleJsonData.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using Midterm;
+using Newtonsoft.Json;
+
+namespace Models
+{
+    class HandleJsonData
+    {
+        public static string filePath = @"C:\Users\test\Documents\GrandCircus\Midterm\Midterm\products.json";
+        public static List<Product> ProductList;
+        public static void GetProducts()
+        {
+            using (StreamReader file = File.OpenText(filePath))
+            {
+                JsonSerializer serializer = new JsonSerializer();
+                //var test = serializer.Deserialize(file, typeof(Product));
+                //ProductList.Add(serializer.Deserialize(file, typeof(Product)));
+                //Product movie2 = (Product)serializer.Deserialize(file, typeof(Product));
+            }
+        }
+    }
+}
--- Midterm/Program.cs
+++ Midterm/Program.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,7 @@ namespace Midterm
     {
         static void Main(string[] args)
         {
+            HandleJsonData.GetProducts();
         }
     }
 }
